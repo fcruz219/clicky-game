f2baa8261a8ba727ecd7da1a71d6d7df96478481
Francisco <fcruz219@gmail.com>
2019-04-24 20:43:36 -0400

    images show on page, need clicks to do something

---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <span onClick={() => props.removeFriend(props.id)} className="select">
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+    </div>
+    </span>
+  );
+}
+
+export default FriendCard;
